dc907d5170ebc752d597f9797b5144f9ef165d97
Onovo-fortune20 <onovojr@gmail.com>
2023-04-11 20:13:36 +0000

    11

--- script.js
+++ script.js
@@ -31,9 +31,39 @@
        document.querySelector(".registration-form #login-btn").addEventListener("click",function(){
         let inputCaptchaValue = document.querySelector(".registration-form  .captcha input").value;
         if(inputCaptchaValue === captchaValue){
-           swal("","Logging In!", "success");
+           swal("","You're human, Input OTP!", "success");
+           window.location.assign("verify.html")
         } else{
             swal("Invalid captcha");
         }
    });
+   var pass = document.getElementById("password");
+ var msg = document.getElementById("message");
+ var str = document.getElementById("strength");
+
+ pass.addEventListener('input', () =>{
+    if(pass.value.length>0){
+        msg.style.display = "block";
+    }
+    else{
+        msg.style.display ="none"
+    }
+    if(pass.value.length < 4){
+        str.innerHTML = "weak";
+        pass.style.borderColor = "red";
+        msg.style.color = "red";
+    }
+    else if(pass.value.length >= 4 && pass.value.length <8){
+        str.innerHTML = "medium";
+        pass.style.borderColor = "yellow";
+        msg.style.color = "yellow";
+    }
+    else if (pass.value.length >= 8){
+    str.innerHTML = "strong";
+    pass.style.borderColor = "green";
+    msg.style.color = "green";
+    }
+ })
+
+
 })();
