520bdaaafe760a4125bdd18913172ea60e062b70
Onovo-fortune20 <onovojr@gmail.com>
2023-04-16 18:43:37 +0000

    LATEST

--- script.js
+++ script.js
@@ -37,33 +37,83 @@
             swal("Invalid captcha");
         }
    });
-   var pass = document.getElementById("password");
- var msg = document.getElementById("message");
- var str = document.getElementById("strength");
+//    var pass = document.getElementById("password");
+//  var msg = document.getElementById("message");
+//  var str = document.getElementById("strength");
 
- pass.addEventListener('input', () =>{
-    if(pass.value.length>0){
-        msg.style.display = "block";
-    }
-    else{
-        msg.style.display ="none"
-    }
-    if(pass.value.length < 4){
-        str.innerHTML = "weak";
-        pass.style.borderColor = "red";
-        msg.style.color = "red";
-    }
-    else if(pass.value.length >= 4 && pass.value.length <8){
-        str.innerHTML = "medium";
-        pass.style.borderColor = "yellow";
-        msg.style.color = "yellow";
-    }
-    else if (pass.value.length >= 8){
-    str.innerHTML = "strong";
-    pass.style.borderColor = "green";
-    msg.style.color = "green";
-    }
- })
+//  pass.addEventListener('input', () =>{
+//     if(pass.value.length>0){
+//         msg.style.display = "block";
+//     }
+//     else{
+//         msg.style.display ="none"
+//     }
+//     if(pass.value.length < 4){
+//         str.innerHTML = "weak";
+//         pass.style.borderColor = "red";
+//         msg.style.color = "red";
+//     }
+//     else if(pass.value.length >= 4 && pass.value.length <8){
+//         str.innerHTML = "medium";
+//         pass.style.borderColor = "yellow";
+//         msg.style.color = "yellow";
+//     }
+//     else if (pass.value.length >= 8){
+//     str.innerHTML = "strong";
+//     pass.style.borderColor = "green";
+//     msg.style.color = "green";
+//     }
+//  })
+ let timeout;
+
+        // traversing the DOM and getting the input and span using their IDs
+
+        let password = document.getElementById('password')
+        let passwordconfirm = document.getElementById('conpassword')
+        let strengthBadge = document.getElementById('strength')
+        let passwordstatement = document.getElementById("passwordchecker")
+
+        // The strong and weak password Regex pattern checker
+
+        let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+        let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))')
+
+        function StrengthChecker(PasswordParameter) {
+            // We then change the badge's color and text based on the password strength
+
+            if (strongPassword.test(PasswordParameter)) {
+                strengthBadge.style.color = "green"
+                strengthBadge.textContent = 'Strong'
+            }
+            else if (PasswordParameter <= 0) {
+                strengthBadge.textContent = ''
+            }
+            else if (mediumPassword.test(PasswordParameter)) {
+                strengthBadge.style.color = 'blue'
+                strengthBadge.textContent = 'Medium'
+            } else {
+                strengthBadge.style.color = 'red'
+                strengthBadge.textContent = 'Weak'
+            }
+        }
+
+        // Adding an input event listener when a user types to the  password input 
+
+        password.addEventListener("input", () => {
+
+
+            // strengthBadge.style.display= 'block'
+            clearTimeout(timeout);
+
+            //We then call the StrengChecker function as a callback then pass the typed password to it
+
+            timeout = setTimeout(() => StrengthChecker(password.value), 500);
+
+        });
+
+
+
+
 
 
 })();
